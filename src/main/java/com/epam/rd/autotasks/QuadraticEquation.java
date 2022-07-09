48874c313dd15ca1092ce1e6cd64d41e7e266ae2
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-09 14:34:09 +0300

    QuadraticEquation

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -15,10 +15,13 @@ public class QuadraticEquation {
         if (discriminant<0){
             System.out.println("no roots");
         } else if (discriminant==0) {
-            System.out.println(-b/(2*a));
+            double root=-b/(2*a);
+            System.out.println(root);
         }
             else {
-            System.out.println((-Math.sqrt(discriminant)-b)/(2*a)+" "+(Math.sqrt(discriminant)-b)/(2*a));
+                double root1=(-sqrt(discriminant)-b)/(2*a);
+                double root2=(sqrt(discriminant)-b)/(2*a);
+            System.out.println(root1 + " " + root2);
         }
     }
 }
